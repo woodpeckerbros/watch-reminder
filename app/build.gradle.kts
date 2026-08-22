53816a8a68891219a2fad7d10c9a4081f56bb964
Refael Nakar <refaelnakar@Refaels-MacBook-Pro.local>
2026-08-22 22:42:53 +0300

    Bump Wear app to version 1.03 code 105

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.woodpeckerbros.watchreminder"
         minSdk = 30
         targetSdk = 35
-        versionCode = 104
-        versionName = "1.02"
+        versionCode = 105
+        versionName = "1.03"
     }
 
     compileOptions {
